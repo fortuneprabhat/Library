3b04d9e8e3f63c67990eb58b6f791ee61eb44467
Prabhat Kumar <prabhat.y.kumar@oracle.com>
2021-03-27 23:48:31 +0530

    Lombok Integration.

--- src/main/java/com/book/Library/model/Book.java
+++ src/main/java/com/book/Library/model/Book.java
@@ -8,7 +8,14 @@ import javax.validation.constraints.NotBlank;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Book {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,44 +25,5 @@ public class Book {
 	String title;
 	@NotBlank
 	@JsonProperty("author")
-	String author;
-	
-	public Book() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public Book(int id, String title, String author) {
-		this.id = id;
-		this.title = title;
-		this.author = author;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getAuthor() {
-		return author;
-	}
-
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-
-	@Override
-	public String toString() {
-		return "Book [id=" + id + ", title=" + title + ", author=" + author + "]";
-	}	
+	String author;	
 }
